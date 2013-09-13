b74911d094589444a9ba8684a7a72cfe5dfc462c
Matt Gaidica <mgaidica@med.umich.edu>
2013-09-13 13:58:09 -0400

    two copies in batch

--- backup.bat
+++ backup.bat
@@ -1,5 +1,7 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+for %%* in (.) do set CurrDirName=%%~n*
 
-7za a -tzip -xr!backup "backup/archive-%mydate%_%mytime%"
+7za a -tzip -xr!backup "backup\%CurrDirName%--%mydate%_%mytime%"
+7za a -tzip -xr!backup "C:\Users\Leventhal1\Documents\My Box Files\Leventhal1\%CurrDirName%--%mydate%_%mytime%"
